8a014e967679bdcf287d4a43439f7e140fefe488
Papirmaage <99833968+OLaursen@users.noreply.github.com>
2023-06-23 12:26:13 +0200

    Getting the damn thing to start
    As node_modules is ignored by git, then you should run "npm install" everytime you start working on this project,

--- living_portfolio_website/src/App.js
+++ living_portfolio_website/src/App.js
@@ -1,21 +1,17 @@
-import { useEffect } from "react";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import {homepage} from './Pages/Home'
+
 
 function App() {
   //Makes browser declare page.
-  useEffect(() => {
-    document.title = "LP - Home";
-  });
+
 
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<homepage />} />
-        </Routes>
-        <Footer />
-      </Router>
+    <div> Theme buttons place</div>
+    <div> Main content placeholder</div>
     </>
+      
   );
 }
+export default App
--- living_portfolio_website/src/pages/Home.js
+++ living_portfolio_website/src/pages/Home.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
 const Home = () => {
-    
+    <>
+    <div className="theme-picker">
+        <button>Light</button>
+        <button>Dark</button>
+    </div>
+        
+    <div className="dynamic-view">
+        I'm the content box
+    </div>
+    </>
 
 }
 export default Home
