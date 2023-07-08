e184cc3f153a127f305d749752d1a1664b45e5b8
Papirmaage <olau@itu.dk>
2023-07-08 13:48:43 +0200

    Update to contentbox

--- living_portfolio_website/src/components/mainview/Contentbox.jsx
+++ living_portfolio_website/src/components/mainview/Contentbox.jsx
@@ -2,33 +2,46 @@ import React, { useState } from "react";
 import "./content-box-styles.css"
 import { Link } from "react-router-dom";
 
-function mainview() {
-    let [sidetext, setSideText] = useState(0);
+function MainView() {
+    const [sidetext, setSideText] = useState("");
+    const handleClick =  (text) => { setSideText(text) };
 
 
     return (
         <>
         <div className="outer-border">
+            <div className="landingpage-menu">
+
             <p className="Title-Name">Oliver Laursen</p>  
             <p className="occupation"> Student at IT-University Copenhagen. </p>
         
         <ul className="main-menu">
             <li>
-            <button className="projects-button" onClick={() => setSideText="1"}>
+            <button className="projects-button" onClick={() => handleClick(
+                <p className="project-side-text"> Currently the portfolio itself is my only project. But there's more to come soon.</p>
+                )}>
             <p className="projects-text">My projects</p>
                 </button>
             </li>
             <li>
-                <button to="/contact" onClick={() => setSideText="2"}>
+                <button to="/contact" onClick={() => handleClick( 
+                    <div className="contact-side-text">
+                                            <p className="contact-email"><strong>Email:</strong> oliverlaursen1998gmail.com </p>
+                                            <p className="contact-linkedin"> <strong> Linked in:</strong> <a className="linkedin-link" href="https://dk.linkedin.com/in/oliver-laursen-swu2022"> My Profile </a></p>
+                                            </div>
+                                                                    )}>
                 <p className="contact-info">Contact info</p>
                 </button>
             </li>
-            <li>
+            <div className="landingpage-text">
                 {sidetext}
-            </li>
+            </div>
+            
         </ul>
+
+                                                                        </div>
         </div>
         </>
     );
 };
-export default mainview;
+export default MainView;
